af6b4cf17a5a6521327036522548278d580f7181
Michael Reinsch <michael@movingfast.io>
2018-09-19 16:13:00 +0200

    use separate dir for logrotate conf

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,9 @@
 
 addgroup -g $LOG_GROUP_ID box
 adduser -s /bin/sh -D -H -u $LOG_USER_ID -S -G box box
+mkdir -p /etc/logrotate
+cat /logrotate.tpl.conf | envsubst > /etc/logrotate/logrotate.conf
 
-cat /logrotate.tpl.conf | envsubst > /etc/logrotate.conf
-
-echo "$CRON_SCHEDULE /usr/sbin/logrotate /etc/logrotate.conf" | crontab -
+echo "$CRON_SCHEDULE /usr/sbin/logrotate /etc/logrotate/logrotate.conf" | crontab -
 
 exec tini $@
